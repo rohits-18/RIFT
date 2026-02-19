c53001f4edcd503a116fb49f87e4ede7c12ae4af
Rohit <dhangarrohit5276@gmail.com>
2026-02-19 19:59:41 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,2 +1,12 @@
-def greet(name)
-    print("Hello", name)
+def calculate_average(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    
+    average = total / len(numbers)
+    return average
+
+
+data = input("Enter numbers separated by space: ").split()
+
+print("Average is:", calculate_average(data))
